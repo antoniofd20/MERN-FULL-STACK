d234e57827d6c3ad57db1cc969e2cb1444942c57
antoniofd20 <antonioflodiaz@gmail.com>
2020-11-06 13:31:10 -0600

    eliminar, crear,listar y seleccionar proyectos

--- cliente/src/components/tareas/ListadoTareas.jsx
+++ cliente/src/components/tareas/ListadoTareas.jsx
@@ -1,8 +1,20 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useContext } from 'react'
 
 import Tarea from './Tarea';
 
+import proyectoContext from '../../context/proyectos/proyectoContext';
+
 const ListadoTareas = () => {
+
+    // Obtener el state del formulario
+    const proyectosContext = useContext(proyectoContext);
+    const { proyecto, eliminarProyecto } = proyectosContext;
+
+    // Si no hay proyecto seleccionado
+    if(!proyecto) return <h2>Selecciona un Proyecto</h2>
+
+    // Array destructuring para extraer el proyecto actual
+    const [ proyectoActual ] = proyecto;
 
     const tareasProyecto = [
         {nombre: 'Elegir plataforma', estado: true},
@@ -10,9 +22,14 @@ const ListadoTareas = () => {
         {nombre: 'Elegir xd', estado: false},
         {nombre: 'Elegir host', estado: true}
     ]
+
+    const onClickEliminar = () => {
+        eliminarProyecto(proyectoActual.id)
+    }
+
     return (
         <Fragment>
-            <h2>Proyecto: Tienda Virtual</h2>
+            <h2>Proyecto: {proyectoActual.nombre}</h2>
             
             <ul className="listado-tareas">
                 {tareasProyecto.length === 0
@@ -28,6 +45,7 @@ const ListadoTareas = () => {
             <button
                 type="button"
                 className="btn btn-eliminar"
+                onClick={onClickEliminar}
             >Eliminar Proyecto &times;</button>
         </Fragment>
     );
--- cliente/src/context/proyectos/proyectoState.jsx
+++ cliente/src/context/proyectos/proyectoState.jsx
@@ -8,7 +8,9 @@ import {
     FORMULARIO_PROYECTO, 
     OBTENER_PROYECTOS,
     AGREGAR_PROYECTO,
-    VALIDAR_FORMULARIO
+    VALIDAR_FORMULARIO,
+    PROYECTO_ACTUAL,
+    ELIMINAR_PROYECTO
 } from '../../types';
 
 const ProyectoState = props => {
@@ -23,7 +25,8 @@ const ProyectoState = props => {
     const initialState = {
         proyectos: [],
         formulario: false,
-        errorformulario: false
+        errorformulario: false,
+        proyecto: null
     }
 
     // Dispatch para ejecutar las acciones
@@ -61,16 +64,35 @@ const ProyectoState = props => {
         })
     }
 
+    // Selecciona el proyecto que el usuario dio click 
+    const proyectoActual = proyectoId => {
+        dispatch({
+            type: PROYECTO_ACTUAL,
+            payload: proyectoId
+        })
+    }
+
+    // Eliminar un proyecto
+    const eliminarProyecto = proyectoId => {
+        dispatch({
+            type: ELIMINAR_PROYECTO,
+            payload: proyectoId
+        })
+    }
+
     return (
         <proyectoContext.Provider
             value={{
                 proyectos: state.proyectos,
                 formulario: state.formulario,
                 errorformulario: state.errorformulario,
+                proyecto: state.proyecto,
                 mostrarFormulario,
                 obtenerProyectos,
                 agregarProyecto,
-                mostrarError
+                mostrarError,
+                proyectoActual,
+                eliminarProyecto
             }}
         >
             {props.children}
